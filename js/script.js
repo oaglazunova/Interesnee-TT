6f4ab3355c8e7761dc87e801cc459ed8db097647
oaglazunova <oaglazunova@gmail.com>
2015-09-05 16:54:33 +0300

    Initial

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// Hamburger icon
+var toggler = document.getElementById('toggler');
+toggler.onclick = function(e){
+  e.preventDefault();
+  toggler.classList.toggle('toggler--close');
+}
+// Hamburger icon end
