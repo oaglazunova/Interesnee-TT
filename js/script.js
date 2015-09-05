c2f5c980bcc19a5512668a01c6684b42a2cea416
oaglazunova <oaglazunova@gmail.com>
2015-09-06 00:14:56 +0300

    Charts section finished

--- js/script.js
+++ js/script.js
@@ -1,7 +1,20 @@
 // Hamburger icon
 var toggler = document.getElementById('toggler');
-toggler.onclick = function(e){
+toggler.onclick = function (e) {
+    e.preventDefault();
+    toggler.classList.toggle('toggler--close');
+  }
+  // Hamburger icon end
+
+/*
+var toggler = document.getElementById('toggler');
+var header = document.getElementById('header');
+var menu = document.getElementById('menu');
+
+toggler.onclick = function (e) {
   e.preventDefault();
-  toggler.classList.toggle('toggler--close');
+  toggler.classList.toggle('menu-toggler--close');
+  menu.classList.toggle('main-nav--vertical');
+  header.classList.toggle('page-header__menu--blue');
 }
-// Hamburger icon end
+*/
