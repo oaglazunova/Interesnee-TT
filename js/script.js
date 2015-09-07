b3cccbd400ad1d4399b9b866dd4de8b204432849
oaglazunova <oaglazunova@gmail.com>
2015-09-07 17:52:15 +0300

    ...

--- js/script.js
+++ js/script.js
@@ -1,22 +1,24 @@
 var toggler = document.getElementById('toggler');
-var menu = document.getElementById("menu");
 
 toggler.onclick = function (e) {
-    e.preventDefault();
-    toggler.classList.toggle('toggler--close');
-  menu.classList.toggle("main-nav__vertical");
-  }
-  
-
-/*
-var toggler = document.getElementById('toggler');
-var header = document.getElementById('header');
-var menu = document.getElementById('menu');
-
-toggler.onclick = function (e) {
+  var menu = document.getElementById("menu");
   e.preventDefault();
-  toggler.classList.toggle('menu-toggler--close');
-  menu.classList.toggle('main-nav--vertical');
-  header.classList.toggle('page-header__menu--blue');
+  toggler.classList.toggle('toggler--close');
+  menu.classList.toggle("main-nav__vertical");
 }
-*/
+
+var toggleUserMenu = document.getElementById("dropdown--user");
+
+toggleUserMenu.onclick = function (e) {
+  
+var menuUser = document.getElementById("menu--user");
+  console.log("click");
+  e.preventDefault();
+  menuUser.classList.toggle("main-nav__dropdown-menu--show");
+}
+
+window.addEventListener("keydown", function (event) {
+  if (event.keyCode == 27 && menuUser.classList.contains("main-nav__dropdown-menu--show")) {
+    menuUser.classList.remove("main-nav__dropdown-menu--show");
+  }   
+}, false);
